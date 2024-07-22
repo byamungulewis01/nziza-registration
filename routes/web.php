96c02252466b757c508a27705b96b9c875c6c2a5
byamungulewis01 <ntwarilebon09@gmail.com>
2024-07-22 17:29:24 +0200

    update success

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::controller(RegistrationController::class)->group(function () {
     Route::get('/', 'index')->name('index');
     Route::post('/register', 'register')->name('register');
-    Route::get('/success', 'success')->name('success');
+    // Route::get('/success', 'success')->name('success');
+    Route::get('/civil-3d-success', 'success')->name('success');
     Route::get('/short-training', 'training_registration')->name('training_registration');
     Route::post('/short-training', 'training_registration_store')->name('training_registration_store');
 });
