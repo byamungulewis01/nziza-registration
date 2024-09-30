73e617d968b4e2b24fe7b3e7d11c1cf67103199b
byamungulewis01 <ntwarilebon09@gmail.com>
2024-09-30 12:11:15 +0200

    add midas success page

--- resources/views/short-training-success.blade.php
+++ resources/views/short-training-success.blade.php
@@ -7,12 +7,29 @@
             <div class="text-center mt-5 mb-3 pb-1">
                 <span class="badge bg-label-primary fs-12">Thank you for Registering</span>
             </div>
-
             <p class="text-center mb-4 mb-lg-5 pb-md-3">
-                Welcome to the Midas Civil 2024 Bridge Design Training!
-                </br></br>
-                <a href="https://wa.me/255775236096" class="btn btn-success">Click here to WhatsApp Us for more info</a>
+                Welcome to the Midas Civil 2024 Bridge Design Training! <br>
+               <strong> Your registration is confirmed!</strong>
+            <h5 class="text-center mt-0">What's Next?</h5>
+            <div class="text-center mt-0">
+                To help you prepare, <a target="_blank" href="https://nzizaglobal.co.tz/wp-content/uploads/2024/09/Curriculum-Bridge-Design-Training.pdf">Click here to download the training curriculum.</a> <br>
+                Please ensure to deposit your payment before <strong>October 14, 2024</strong>, to avoid losing your seat.
+                <br>
+                Stay Connected! <a target="_blank" href="https://chat.whatsapp.com/LuQa4Nf0xz6IWucpr7O4AK">Click here to Join our WhatsApp Group</a>
+
+            </div>
+            <h5 class="text-center mt-3">Need Help?</h5>
+            <div class="text-center mt-0">
+                For questions, contact us at: <br>
+                <strong>Email:</strong> info@nzizaglobal.co.tz <br>
+                <strong>Phone:</strong> +255 752 303 123 <br><br>
+                <strong>Thank you again! We can’t wait to see you there!</strong>
+
+
+            </div>
+
             </p>
+
 
         </div>
     </section>
